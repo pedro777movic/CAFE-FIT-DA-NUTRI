11e50ea2a62b6d4c5a211f1a749fc116a43ed51e
Pedro Barbosa Barbosa <pedrobarbosa2971@gmail.com>
2025-12-27 15:58:15 +0000

    Adicione um subtítulo abaixo de "+ 500 receitas fit", escrito: "Você pod

--- src/components/landing/showcase.tsx
+++ src/components/landing/showcase.tsx
@@ -118,6 +118,9 @@ export function Showcase() {
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">
               + 500 Receitas Fit
             </h2>
+            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              Você pode comer isso!
+            </p>
           </div>
         </ScrollReveal>
         <div className="flex flex-col gap-4">
