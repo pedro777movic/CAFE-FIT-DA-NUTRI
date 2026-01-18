ec19248d0184d251a11f96b251dcfee1bce7cf57
Pedro Barbosa Barbosa <pedrobarbosa2971@gmail.com>
2026-01-18 19:52:40 +0000

    Você vai adicionar 3 códigos pixel nessa página: (Instale perfeitamente,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
 import { ActivityNotifications } from '@/components/activity-notifications';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'O Fim da Luta com a Comida.',
@@ -21,9 +22,58 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1125280262863080&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         {children}
         <Toaster />
         <ActivityNotifications />
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1125280262863080');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* UTMIfy Pixel Code */}
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "696d3535e2c8a11ef0bed7d4";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
+
+        {/* UTMs Script */}
+        <Script
+          id="utmify-utms"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck=""
+          data-utmify-prevent-subids=""
+          async
+          defer
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
